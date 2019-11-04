10f55f377eca9900dd81577a9eb4462eb8e85d9d
219003113 <42488276+219003113@users.noreply.github.com>
2019-11-04 12:09:02 +0530

    Update salesiq.js

--- js/salesiq.js
+++ js/salesiq.js
@@ -1,2 +1,9 @@
 
 var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"1e32222cfa9110092c486e74fe31d0d3c656f760440918eb58877139ca6c516d8749c0d1dd1c471778d5550e66e590eb", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+$zoho.salesiq.ready = function (embedinfo) {
+    $zoho.salesiq.visitor.trigger(function (triggername, visitorinfo) {
+        if (triggername == 'Alerter') {
+            console.log("Alerter");
+        }
+    });
+}
